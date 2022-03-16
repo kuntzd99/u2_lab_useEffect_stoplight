87e7a20ce85e035662e9968f2338f299f1f7041a
Daniel Kuntz <kuntzd99@gmail.com>
2022-03-16 10:53:50 -0500

    Lab completed

--- src/components/Highway.jsx
+++ src/components/Highway.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import RaceCar from './RaceCar'
 
 const imgUrls = [
@@ -6,9 +6,22 @@ const imgUrls = [
   `https://res.cloudinary.com/ahonore42/image/upload/v1614648099/ga/greencoupe_hr19ae.png`,
 ]
 
-const Highway = (props) => {
+const Highway = ({lights}) => {
   const [racing, setRacing] = useState('cars')
   // your code here
+
+  const toggleRacing = () => {
+    if (racing==='cars' && lights.green) {
+      setRacing('cars race')
+    }
+    if (racing==='cars race' && !lights.green) {
+      setRacing('cars')
+    }
+  }
+
+useEffect(() => {
+  toggleRacing()
+}, [lights])
 
   return (
     <div className="highway">
--- src/components/StopLight.jsx
+++ src/components/StopLight.jsx
@@ -1,20 +1,39 @@
 const StopLight = (props) => {
+  console.log(props.lights)
 
   return (
     <div className="action-area">
       <h1>Stoplight useEffect</h1>
       <div className="stoplight-container">
         <div className="stoplight">
-          <div></div>
-          <div></div>
-          <div></div>
+          <div className={props.lights.red ? 'red' : undefined}></div>
+          <div className={props.lights.yellow ? 'yellow' : undefined}></div>
+          <div className={props.lights.green ? 'green' : undefined}></div>
         </div>
         <div>
           <button
             className="countdown"
-            // onCLick here
+            onClick={() => {
+              if (props.lights.red === true && 
+                props.lights.yellow === true && 
+                props.lights.green === true) {
+                  props.setLights({red: false, yellow: false, green: false})
+              } else if (props.lights.red === true && 
+                props.lights.yellow === true && 
+                props.lights.green === false) {
+                  props.setLights({red: true, yellow: true, green: true})
+              } else if (props.lights.red === true && 
+                props.lights.yellow === false && 
+                props.lights.green === false) {
+                  props.setLights({red: true, yellow: true, green: false})
+              } else if (props.lights.red === false && 
+                props.lights.yellow === false && 
+                props.lights.green === false) {
+                  props.setLights({red: true, yellow: false, green: false})
+                }
+            }}
           >
-          Countdown
+          {props.lights.red && props.lights.yellow && props.lights.green ? "Reset" : "Countdown"}
           </button>
         </div>
       </div>
